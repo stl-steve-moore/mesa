ab9ae4e6d8582cb3fe4430f3dfa0258f620525ce
Steve Moore <moores@mir.wustl.edu>
2013-11-05 16:17:59 -0600

    Rename a variable that was used both outside and inside a loop. Earlier compiler versions let us get away with this.

--- apps/of_sched_procedure/of_sched_procedure.cpp
+++ apps/of_sched_procedure/of_sched_procedure.cpp
@@ -473,7 +473,7 @@ scheduleProcedures(MMWL& mwl, st_dbInterface& db,
   db.dbInterface.select(s, cv3, scheduleSPS, &db);
 
   int spsSize = db.scheduleVector.size();
-  for(int x = 0; x < spsSize; x++){
+  for(int ix = 0; ix < spsSize; ix++){
     MString x = db.scheduleVector.back().universalServiceID();
     db.mwl.requestedProcedureDescription(x.getToken('^', 1));
 
